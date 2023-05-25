39b36b36c4b8c88b9e27c4549acbf729c7f94398
NiezSellami <sellami-niez@hotmail.fr>
2023-05-26 01:03:15 +0200

    angular 11 migration

--- src/app/album/album.page.spec.ts
+++ src/app/album/album.page.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { AlbumPage } from './album.page';
@@ -7,7 +7,7 @@ describe('AlbumPage', () => {
   let component: AlbumPage;
   let fixture: ComponentFixture<AlbumPage>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AlbumPage ],
       imports: [IonicModule.forRoot()]
