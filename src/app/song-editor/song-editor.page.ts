3c1f5fd26051baf06443fe7452f6d5cb20b9ce91
NiezSellami <sellami-niez@hotmail.fr>
2023-06-02 16:41:48 +0200

    footer modal

--- src/app/song-editor/song-editor.page.ts
+++ src/app/song-editor/song-editor.page.ts
@@ -9,6 +9,115 @@ export class SongEditorPage implements OnInit {
   isModalOpen = false;
   @ViewChild('footerToolbar', { read: ElementRef }) toolbar: ElementRef;
   @ViewChild('modal', { read: ElementRef }) modal: ElementRef;
+
+  public bpmPicker = [
+    {
+      name: 'bpm',
+      options: [
+        {
+          text: '100 bpm',
+          value: '100',
+        },
+        {
+          text: '120 bpm',
+          value: '120',
+        },
+        {
+          text: '140 bpm',
+          value: '140',
+        },
+        {
+          text: '160 bpm',
+          value: '160',
+        },
+      ],
+    },
+  ];
+
+  public bpmButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+    },
+    {
+      text: 'Confirm',
+      handler: (value) => {
+        this.bpmValue=value.bpm;
+      },
+    },
+  ];
+  public speedPicker = [
+    {
+      name: 'speed',
+      options: [
+        {
+          text: '1 x',
+          value: '1',
+        },
+        {
+          text: '2 x',
+          value: '2',
+        },
+        {
+          text: '3 x',
+          value: '3',
+        },
+      ],
+    },
+  ];
+
+  public speedButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+    },
+    {
+      text: 'Confirm',
+      handler: (value) => {
+        this.speedValue=value.speed;
+      },
+    },
+  ];
+  public majorPicker = [
+    {
+      name: 'major',
+      options: [
+        {
+          text: 'EB major',
+          value: 'EB',
+        },
+        {
+          text: 'CE major',
+          value: 'CE',
+        },
+      ],
+    },
+  ];
+
+  public majorButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel',
+    },
+    {
+      text: 'Confirm',
+      handler: (value) => {
+        this.majorValue=value.major;
+      },
+    },
+  ];
+  bpmValue = {
+    text: '100 bpm',
+    value: '100',
+  };
+  speedValue = {
+    text: '2 x',
+    value: '2',
+  };
+  majorValue = {
+    text: 'EB major',
+    value: 'EB',
+  };
   constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
