0ae556edf59a623c9bbe95135f70706aee9e489f
NiezSellami <sellami-niez@hotmail.fr>
2023-06-02 12:41:05 +0200

    add dismiss feature to the modal add grid view to header buttons

--- src/app/song-editor/song-editor.page.ts
+++ src/app/song-editor/song-editor.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-song-editor',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./song-editor.page.scss'],
 })
 export class SongEditorPage implements OnInit {
-
-  constructor() { }
+  isModalOpen = false;
+  @ViewChild('footerToolbar', { read: ElementRef }) toolbar: ElementRef;
+  @ViewChild('modal', { read: ElementRef }) modal: ElementRef;
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
   }
-
+  setOpen(isOpen: boolean) {
+    const toolbarHeight = this.toolbar.nativeElement.offsetHeight;
+    this.renderer.setStyle(this.modal.nativeElement, 'bottom', `${toolbarHeight}px`);
+    this.isModalOpen = isOpen;
+  }
+  checkModal(){
+    this.isModalOpen==true?this.setOpen(false):null;
+  }
+  
 }
